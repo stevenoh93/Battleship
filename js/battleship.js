67a51dc0c809059ff9849c89d863e00b871e4849
stevenoh93 <stevenoh93@gmail.com>
2015-05-07 16:49:47 -0400

    Added small ship

--- js/battleship.js
+++ js/battleship.js
@@ -6,6 +6,8 @@ var cameraControls;
 
 var waters = [];
 
+var group;
+
 function init() {
 	var innerWidth = window.innerWidth;
     var innerHeight = window.innerHeight;
@@ -18,12 +20,15 @@ function init() {
 
     //Camera:
     camera = new THREE.PerspectiveCamera( 40, aspectRatio, 0.1, 10000 );
-    camera.position.set( 0, 500, 500 );
+    camera.position.set( 0, 200, 400 );
     camera.up = new THREE.Vector3(0,0,1);
     camera.lookAt(new THREE.Vector3(0,0,0));
 
     //CameraControls:
     cameraControls = new THREE.OrbitControls( camera, renderer.domElement );
+
+    group = new THREE.Group();
+    // group.position.y=50;
 	
 	var container = document.getElementById( "container" );
     container.appendChild( renderer.domElement );    
@@ -36,11 +41,13 @@ function drawScene() {
 	scene.add( ambientLight );
 
 	makeBackground();
-    
+	makeShips();
     // Draw waters
 	for (var i=0; i<27; i++) {
 		scene.add( waters[i] );
 	}
+
+	scene.add( group );
     
     debugaxis(1000);
 }
@@ -56,11 +63,11 @@ function render() {
 }
 
 function makeBackground() {
-    var boxGeo = new THREE.BoxGeometry( 100, 100, 10 );
-    var texture = THREE.ImageUtils.loadTexture("textures/water.jpg");
+    var boxGeo = new THREE.BoxGeometry( 200, 200, 10 );
+    var texture = THREE.ImageUtils.loadTexture( "textures/water.jpg" );
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(3,3);
+    texture.repeat.set( 1, 1 );
     var boxMaterial = new THREE.MeshBasicMaterial({
         map:texture,
         side:THREE.DoubleSide,
@@ -68,16 +75,46 @@ function makeBackground() {
         opacity: 0.5, 
         color: 0xFFFFFF
     });
-	for (var i=-10; i<=10; i+=10) {
-        for (var j=-10; j<=10; j+=10) {
+	for (var i=-1; i<=1; i++) {
+        for (var j=-1; j<=1; j++) {
             var box = new THREE.Mesh( boxGeo, boxMaterial );
-            box.position.set(i*10, j*10, 0);
+            box.position.set(i*200, j*200, 0);
     		waters.push( box );
         }
 	}
-    
 }
 
+function makeShips() {
+	var extrudeSettings = { amount: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
+
+	// Large ship
+	var smallShipPts = [];
+	smallShipPts.push( new THREE.Vector2 ( 0, 20 ) );
+	smallShipPts.push( new THREE.Vector2 ( -8, 0 ) );
+	smallShipPts.push( new THREE.Vector2 ( -8, -20 ) );
+	smallShipPts.push( new THREE.Vector2 ( 8, -20 ) );
+	smallShipPts.push( new THREE.Vector2 ( 8, 0 ) );
+	var smallShipShape = new THREE.Shape( smallShipPts );
+	
+	var geometry = new THREE.ExtrudeGeometry( smallShipShape, extrudeSettings );
+	var texture = new THREE.ImageUtils.loadTexture( "textures/ship_side.jpg" );
+	texture.wrapS = THREE.ClampToEdgeWrapping;
+	texture.wrapT = THREE.ClampToEdgeWrapping;
+	texture.minFilter = THREE.NearestFilter;
+	texture.repeat
+	var material = new THREE.MeshBasicMaterial({
+        map:texture,
+        side:THREE.DoubleSide
+    });
+    var mesh = THREE.SceneUtils.createMultiMaterialObject(geometry, [material, new     THREE.MeshBasicMaterial({
+	    color: 0x000000,
+	    wireframe: true,
+	    transparent: true
+	})]);
+	// var mesh = new THREE.Mesh( geometry, material );
+	mesh.position.set( 0, 0, 1 );
+	group.add( mesh );
+}
 
 var debugaxis = function(axisLength){
     //Shorten the vertex function
